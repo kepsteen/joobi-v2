709a950d7d1611d5b9540a804f54586f4731f4da
Cody Epstein <codyepstein@gmail.com>
2024-11-18 21:20:12 -0800

    add button to show modal

--- app/home/applications/page.tsx
+++ app/home/applications/page.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import ApplicationsTable from "@/components/ApplicationsTable/ApplicationsTable";
+import { Button } from "@/components/Button/Button";
 import { Card, CardContent } from "@/components/Card/Card";
 import Heading from "@/components/Heading/Heading";
+import NewApplicationModal from "@/components/NewApplicationModal/NewApplicationModal";
 import { ListPlus } from "lucide-react";
 
 export default function Applications() {
@@ -8,12 +12,26 @@ export default function Applications() {
 		<section className="h-full flex flex-col">
 			<div className="shrink-0 flex justify-between items-center mb-4">
 				<div>
-					<Heading headingLevel="h1" className="text-2xl text-neutral mb-2">
+					<Heading
+						headingLevel="h1"
+						className="text-2xl text-neutral mb-2 z-50"
+					>
 						Applications
 					</Heading>
 					<p>Manage your Applications.</p>
 				</div>
-				<ListPlus size={32} color="oklch(var(--p))" />
+				<Button
+					variant="link"
+					onClick={() =>
+						(
+							document.getElementById(
+								"new_application_modal"
+							) as HTMLDialogElement
+						)?.showModal()
+					}
+				>
+					<ListPlus size={32} color="oklch(var(--p))" />
+				</Button>
 			</div>
 			<Card
 				variant="outline"
@@ -24,6 +42,7 @@ export default function Applications() {
 					<ApplicationsTable />
 				</CardContent>
 			</Card>
+			<NewApplicationModal />
 		</section>
 	);
 }
