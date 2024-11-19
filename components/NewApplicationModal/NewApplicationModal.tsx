8b366e8b7d92f1298ef249cddc496f336b3e9db2
Cody Epstein <codyepstein@gmail.com>
2024-11-18 21:53:12 -0800

    add inputs

--- components/NewApplicationModal/NewApplicationModal.tsx
+++ components/NewApplicationModal/NewApplicationModal.tsx
@@ -7,7 +7,7 @@ import { Input } from "../Input/Input";
 export default function NewApplicationModal() {
 	return (
 		<dialog id="new_application_modal" className="modal">
-			<Card variant="outline" className="modal-box border-primary">
+			<Card variant="outline" className="modal-box">
 				<form method="dialog">
 					<Button
 						type="submit"
@@ -18,15 +18,53 @@ export default function NewApplicationModal() {
 					</Button>
 				</form>
 				<CardHeader>
-					<CardTitle className="pr-8 text-primary">
+					<CardTitle className="pr-8 text-neutral">
 						Add New Application
 					</CardTitle>
 				</CardHeader>
 				<CardContent>
-					<form action="">
+					<form action="" className="flex flex-col gap-2">
 						<Label className="text-primary">
 							Stage
-							<Input />
+							<Input
+								name="stage"
+								className="bg-base-100 border border-neutral text-neutral focus:ring focus:ring-primary"
+							/>
+						</Label>
+						<Label className="text-primary">
+							Company
+							<Input
+								name="company"
+								className="bg-base-100 border border-neutral text-neutral focus:ring focus:ring-primary"
+							/>
+						</Label>
+						<Label className="text-primary">
+							Position
+							<Input
+								name="position"
+								className="bg-base-100 border border-neutral text-neutral focus:ring focus:ring-primary"
+							/>
+						</Label>
+						<Label className="text-primary">
+							Link
+							<Input
+								name="link"
+								className="bg-base-100 border border-neutral text-neutral focus:ring focus:ring-primary"
+							/>
+						</Label>
+						<Label className="text-primary">
+							Location
+							<Input
+								name="location"
+								className="bg-base-100 border border-neutral text-neutral focus:ring focus:ring-primary"
+							/>
+						</Label>
+						<Label className="text-primary">
+							Job Description
+							<Input
+								name="job-description"
+								className="bg-base-100 border border-neutral text-neutral focus:ring focus:ring-primary"
+							/>
 						</Label>
 					</form>
 				</CardContent>
